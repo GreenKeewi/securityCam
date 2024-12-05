e6d621164a09aabf0ba7fe445167006f9d833567
GreenKeewi <aliononmars07@gmail.com>
2024-12-04 21:20:23 -0500

    Tuning and Fixing of the Body and Face Tracking plus some logic

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
-recording = True
+detection = True
+detection_stopped_time = None
+timer_started = False
 
 frame_size = (int(cap.get(3)), int(cap.get(4)))
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
@@ -18,7 +20,7 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 7)
-    bodies = face_cascade.detectMultiScale(gray, 1.3, 7)
+    bodies = body_cascade.detectMultiScale(gray, 1.3, 7)
 
     #for (x, y, width, height) in faces:
         #cv2.rectangle(frame, (x, y), (x + width, y + width), (255, 0, 0),3)
@@ -26,7 +28,7 @@ while True:
         #cv2.rectangle(frame, (x, y), (x + width, y + width), (255, 0, 0),3)
 
     if len(faces) + len(bodies) > 0:
-        recording = True
+        detection = True
 
     out.write(frame)
 
@@ -34,7 +36,7 @@ while True:
 
     if cv2.waitKey(1) == ord('q'):
         break
-    
+
 out.release()
 cap.release()
 cv2.destroyAllWindows
